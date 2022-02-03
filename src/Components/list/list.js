8d708ddcc4f2d83b90fd525c2730998a4373d47a
juan bautista fabrizi <juanbautistafabrizi@gmail.com>
2022-02-03 15:59:50 -0300

    Slider done

--- src/Components/list/list.js
+++ src/Components/list/list.js
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 import "./list.scss";
-import {ArrowBackOutlined} from "@material-ui/icons";
-import {ArrowForwardOutlined} from "@material-ui/icons";
-import ListItem from '../listItem/listItem';
-
+import { ArrowBackOutlined } from "@material-ui/icons";
+import { ArrowForwardOutlined } from "@material-ui/icons";
+import ListItem from "../listItem/listItem";
 
 const List = () => {
-  return (<div className='list'>
-      <span className='listTittle'>Continue to watch</span>
-      <div className='wrapper'>
-        <ArrowBackOutlined className='sliderArrow left'/>
-        <div className='container'>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
-          <ListItem/>
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setIsMoved(true);
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+  return (
+    <div className="list">
+      <span className="listTittle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        <ArrowForwardOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
         </div>
-        <ArrowForwardOutlined className='sliderArrow right'/>
       </div>
-  </div>);
+    </div>
+  );
 };
 
 export default List;
--- src/Components/listItem/listItem.js
+++ src/Components/listItem/listItem.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './listItem.scss';
+import {PlayArrow,Add,ThumbUpOutlined,ThumbDownAltOutlined} from '@material-ui/icons';
 
 const ListItem = () => {
   return (
     <div className='listItem'>
-        item
+        <img src="https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2021/12/19/16399524819846.jpg" alt="imagen de pelicula" />
+        <div className='itemInfo'>
+          <div className='icons'>
+            <PlayArrow/>
+            <Add/>
+            <ThumbUpOutlined/>
+            <ThumbDownAltOutlined/>
+          </div>
+        </div>
     </div>
   );
       
